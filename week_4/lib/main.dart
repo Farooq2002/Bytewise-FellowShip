eb7ea91016d10ff08d8ceff9e53f8277cf36c29f
Farooq2002 <fa552991@gmail.com>
2023-04-15 00:54:44 +0500

    animation_work1

--- week_4/lib/main.dart
+++ week_4/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' show pi;
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,49 +8,77 @@ void main() {
   );
 }
 
-const owlUrl =
-    'https://raw.githubusercontent.com/flutter/website/main/src/assets/images/docs/owl.jpg';
-
-class FadeInDemo extends StatefulWidget {
-  const FadeInDemo({Key? key}) : super(key: key);
-
-  @override
-  State<FadeInDemo> createState() => _FadeInDemoState();
-}
-
-class _FadeInDemoState extends State<FadeInDemo> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(children: <Widget>[
-        Image.network(owlUrl),
-        TextButton(
-            child: const Text(
-              'Show Details',
-              style: TextStyle(color: Colors.blueAccent),
-            ),
-            onPressed: () => {}),
-        Column(
-          children: const [
-            Text('Type: Owl'),
-            Text('Age: 39'),
-            Text('Employment: None'),
-          ],
-        )
-      ]),
-    );
-  }
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: FadeInDemo(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    _animation = Tween(begin: 0.0, end: 2 * pi).animate(_controller);
+    _controller.repeat();
+  }
+
+  /*
+  0.0 = 0 degrees
+  0.5 = 180 degrees
+  1.0 = 360 degrees
+*/
+  @override
+  void dispose() {
+    _controller.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Animation'),
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform(
+              transform: Matrix4.identity()..rotateX(_animation.value),
+              alignment: Alignment.center,
+              child: Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                          spreadRadius: 5)
+                    ]),
+              ),
+            );
+          },
         ),
       ),
     );
