438f827dc27632d86240d05f88ca84e0c933994e
Farooq2002 <fa552991@gmail.com>
2023-05-05 17:36:58 +0500

    week456

--- week_4/lib/main.dart
+++ week_4/lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:math' show pi;
-
 import 'package:flutter/material.dart';
+import 'package:week_4/different_animations/first_animation.dart';
 
 void main() {
   runApp(
@@ -15,71 +14,55 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
+class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
-    super.initState();
-    _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 3));
-    _animation = Tween(begin: 0.0, end: 2 * pi).animate(_controller);
-    _controller.repeat();
-  }
-
-  /*
-  0.0 = 0 degrees
-  0.5 = 180 degrees
-  1.0 = 360 degrees
-*/
-  @override
-  void dispose() {
-    _controller.dispose();
-  }
-
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Animation'),
-      ),
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return Transform(
-              transform: Matrix4.identity()..rotateX(_animation.value),
-              alignment: Alignment.center,
-              child: Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.blue,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black.withOpacity(0.5),
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
-                          spreadRadius: 5)
-                    ]),
-              ),
-            );
-          },
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const FirstAnimation()));
+            },
+            child: const Center(
+              child: Hero(
+                  tag: 'splash',
+                  child: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    maxRadius: 120,
+                    backgroundImage: AssetImage('assets/icons/flutter.png'),
+                  )),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text('Tap on the Photo'),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            'HERO ANIMATION',
+            style: TextStyle(fontSize: 20),
+          )
+        ],
       ),
     );
   }
